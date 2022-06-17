939ba5a332e93d5c4b161a5545610881ccc6567d
otajisan <taji.taji.otajisan@gmail.com>
2022-06-17 11:54:24 +0900

    :tada: created by spring initializr

---
+++ src/main/kotlin/io/morningcode/googleadsapitrial/GoogleAdsApiTrialApplication.kt
@@ -0,0 +1,11 @@
+package io.morningcode.googleadsapitrial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleAdsApiTrialApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoogleAdsApiTrialApplication>(*args)
+}
---
+++ src/test/kotlin/io/morningcode/googleadsapitrial/GoogleAdsApiTrialApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.morningcode.googleadsapitrial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoogleAdsApiTrialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
